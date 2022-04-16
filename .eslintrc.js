d673a3a1288fff8b5ea8a9844d393ee100cb0894
Gabor Soos <soos.gabor86@gmail.com>
2022-04-16 15:27:21 +0200

    eslint as js again and add vite again

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  plugins: ['prettier'],
+  extends: ['plugin:vue/essential', 'airbnb/base'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'object-curly-newline': ['error', { consistent: true }],
+    'max-len': ['error', { code: 120 }],
+    'comma-dangle': ['error', 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'prettier/prettier': 'error',
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'implicit-arrow-linebreak': 0,
+    'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
+    'vue/multi-word-component-names': 0
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.{j,t}s?(x)'],
+      env: {
+        mocha: true
+      }
+    }
+  ]
+};
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { createVuePlugin } from 'vite-plugin-vue2';
+
+export default {
+  root: 'src',
+  plugins: [createVuePlugin()]
+};
